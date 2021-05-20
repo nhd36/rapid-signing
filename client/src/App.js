3481932c38116872102c008563e5cfa5bd482ab3
ya332 <alparslanyigitcan@gmail.com>
2021-05-20 02:26:57 -0400

    Add manage view, routing swtich for home, login and register views, add table for the documents, update lifecycle for Manage component to show all documents

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,54 @@
 import './App.css';
-import Manage from './components/Manage';
+
+import Register from "./components/Register";
+import Login from "./components/Login";
+import LandingPage from "./components/LandingPage";
+import Manage from './components/Manage/Manage';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Manage />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="navigation-bar">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/manage">Manage</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* <header className="App-header"></header> */}
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+          <Route exact path="/manage">
+            <Manage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function LandingPage() {
+    return <h2>LandingPage Page</h2>;
+}
+
+export default LandingPage;
