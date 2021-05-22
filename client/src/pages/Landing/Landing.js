713fadd98f1d8bf08287b67976369fb313418063
ya332 <alparslanyigitcan@gmail.com>
2021-05-22 14:48:23 -0400

    Enhance landing page

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import "./Landing.css"
 import Layout from "../Layout";
+import { Link } from 'react-router-dom';
+import FeelingProud from "../../assets/usecases/feeling_proud.svg";
+import SocialGirl from "../../assets/usecases/social_girl.svg";
+import Marketing from "../../assets/usecases/marketing.svg"
+import Influencer from "../../assets/usecases/influencer.svg";
+import OnlineEverywhere from "../../assets/usecases/online_everywhere.svg";
+
 
 class Landing extends React.Component {
   render() {
     return (
       <div>
-        <Layout auth={false}/>
+        <Layout auth={false} />
         <div class="content">
           <div class="hero-image">
             <div class="hero-text">
@@ -22,6 +29,73 @@ class Landing extends React.Component {
             <p>The recipient opens the link and signs</p>
             <p><i>It's that simple</i></p>
 
+          </div>
+        </div>
+        <div class="sections">
+          <div class="section feature">
+            <div>
+              <h2>Meet John, Thesis Student</h2>
+              <p
+              >John needed to get 5 signatures on his thesis approval form. He used RapidSign and shared his form link with the recipients. He managed to gather all his signatures in 2 days.  </p>
+              </div>
+              <div>
+              <img src={FeelingProud} />
+             
+            </div>
+          </div>
+
+          <div class="section feature feature-right">
+            <div>
+              <h2>Meet Alice, Marketing Club President </h2>
+              <p>
+                Alice needed to get her members sign a consent form for an outdoor trip. Alice used RapidSign and her 56 members signed her form in 3 days.
+              </p>
+            </div>
+            <div>
+              <img src={SocialGirl} />
+            </div>
+          </div>
+
+          <div class="section feature">
+            <div>
+              <h2>Meet John, Recruiter</h2>
+              <p>John is a recruiter for a big tech company and used RapidSign to extend the offer letter to the applicant. Applicant signed the form in RapidSign's secure platform. </p>
+            </div>
+            <div>
+              <img src={Marketing} />
+            </div>
+          </div>
+
+          <div class="section feature">
+            <div>
+              <img src={Influencer}/>
+            </div>
+            <div>
+              <h2>Meet Allison, Landlord</h2>
+              <p>Allison is a landlord and used RapidSign to get signatures from her 5 tenants. All of her 5 tenants signed the document using RapidSign under a days, which is much faster than using email.
+              </p>
+            </div>
+          </div>
+
+
+          <div class="bottom-section section">
+            <h2>Supercharge Your Forms with RapidSign Now for Free.
+              <br></br>
+              No credit card required.
+            </h2>
+
+
+            <img src={OnlineEverywhere} />
+            <p>&nbsp;</p>
+            <p>
+              <Link class="get-started-btn" to="/register">Get Started for Free</Link>
+            </p>
+          </div>
+
+          <div class="footer">
+            <div class="copyright">
+              Copyright © 2021 <Link to="/home">RapidSign</Link>. All rights reserved.
+            </div>
           </div>
         </div>
       </div>
