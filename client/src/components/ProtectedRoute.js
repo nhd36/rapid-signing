2d20f3e5f1930717f9b4e4055caeeb21393b121e
ya332 <alparslanyigitcan@gmail.com>
2021-06-04 06:34:22 -0400

    Remove unused jwt_decode

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,5 +1,4 @@
 import { Route, Redirect } from "react-router-dom"
-import jwt_decode from "jwt-decode";
 
 const ProtectedRoute = ({ component: Component, userEmail, ...rest }) => {
     const auth = userEmail !== null ? true : false;
